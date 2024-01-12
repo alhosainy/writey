4a0a8f5c25965f21637df647b17b1aa824b8f3bc
alhosainy <alhosainyyaser@gmail.com>
2024-01-13 01:59:41 +0200

    Update generate_changelog.sh script to create CHANGELOG.md file

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,31 +1,31 @@
 # Set the version number (passed as an argument)
 version_number=$1
 
-# Create the changelog.md file
+# Create the CHANGELOG.md file
 echo "# Changelog" >CHANGELOG.md
-echo "" >>changelog.md
-echo "All notable changes to this project will be documented in this file." >>changelog.md
-echo "" >>changelog.md
-echo "## [Unreleased]" >>changelog.md
-echo "" >>changelog.md
-echo "## [${version_number}] - $(date +'%Y-%m-%d')" >>changelog.md
-echo "### Added" >>changelog.md
+echo "" >>CHANGELOG.md
+echo "All notable changes to this project will be documented in this file." >>CHANGELOG.md
+echo "" >>CHANGELOG.md
+echo "## [Unreleased]" >>CHANGELOG.md
+echo "" >>CHANGELOG.md
+echo "## [${version_number}] - $(date +'%Y-%m-%d')" >>CHANGELOG.md
+echo "### Added" >>CHANGELOG.md
 #! add new features here
-echo "- Feature 1: Description of the new feature." >>changelog.md
-echo "" >>changelog.md
-echo "### Improved" >>changelog.md
+echo "- Feature 1: Description of the new feature." >>CHANGELOG.md
+echo "" >>CHANGELOG.md
+echo "### Improved" >>CHANGELOG.md
 # ! add improvements here
-echo "- Improvement 1: Description of the improvement." >>changelog.md
-echo "" >>changelog.md
-echo "### Fixed" >>changelog.md
+echo "- Improvement 1: Description of the improvement." >>CHANGELOG.md
+echo "" >>CHANGELOG.md
+echo "### Fixed" >>CHANGELOG.md
 # ! add bug fixes here
-echo "- Bug fix 1: Description of the bug fix." >>changelog.md
-echo "- Bug fix 2: Description of the second bug fix." >>changelog.md
-echo "" >>changelog.md
-echo "### Download" >>changelog.md
-echo "- [Download for Android](https://github.com/alhosainy/writey/releases/download/v${version_number}/app-arm64-v8a-release.apk)" >>changelog.md
-echo "- [Download for Windows](https://github.com/alhosainy/writey/releases/download/v${version_number}/writey.rar)" >>changelog.md
-echo "- [Web Version](https://alhosainy.github.io/writey/)" >>changelog.md
-echo "" >>changelog.md
-echo "[Unreleased]: #Unreleased" >>changelog.md
-echo "[${version_number}]: #${version_number}" >>changelog.md
+echo "- Bug fix 1: Description of the bug fix." >>CHANGELOG.md
+echo "- Bug fix 2: Description of the second bug fix." >>CHANGELOG.md
+echo "" >>CHANGELOG.md
+echo "### Download" >>CHANGELOG.md
+echo "- [Download for Android](https://github.com/alhosainy/writey/releases/download/v${version_number}/app-arm64-v8a-release.apk)" >>CHANGELOG.md
+echo "- [Download for Windows](https://github.com/alhosainy/writey/releases/download/v${version_number}/writey.rar)" >>CHANGELOG.md
+echo "- [Web Version](https://alhosainy.github.io/writey/)" >>CHANGELOG.md
+echo "" >>CHANGELOG.md
+echo "[Unreleased]: #Unreleased" >>CHANGELOG.md
+echo "[${version_number}]: #${version_number}" >>CHANGELOG.md
